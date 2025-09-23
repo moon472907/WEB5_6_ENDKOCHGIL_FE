2e2f727a1f1c71937f3bf004bcae5187b5db96ef
taeeun98 <xodms6360@naver.com>
2025-09-23 11:30:43 +0900

    feat: 간편 로그인 logo 추가

--- src/app/login/components/SocialLoginButton.tsx
+++ src/app/login/components/SocialLoginButton.tsx
@@ -1,15 +1,26 @@
-type Provider = "kakao" | "naver" | "google";
+import Image from 'next/image';
 
+type Provider = '카카오' | '네이버' | '구글';
 
 interface SocialLoginButtonProps {
-  provider : Provider;
+  provider: Provider;
+  logoSrc: string;
+  logoWidth: number;
+  logoHeight: number;
   className: string;
 }
 
-function OAuthButton({provider, className}: SocialLoginButtonProps) {
+function SocialLoginButton({ provider, logoSrc, logoWidth, logoHeight, className }: SocialLoginButtonProps) {
   return (
     // TODO : Link? api 호출? onclick? 어떻게 구현되는지 모르겠음 => api 명세서 이후에 작업
-    <button className={`w-full max-w-[328px] h-[48px] rounded-lg shadow-[0_4px_4px_rgba(0,0,0,0.25)] cursor-pointer ${className}`}>{provider}로 로그인하기</button>
-  )
+    <button
+      className={`w-full max-w-[328px] h-[48px] rounded-lg shadow-[0_4px_4px_rgba(0,0,0,0.25)] cursor-pointer ${className}`}
+    >
+      <div className='flex justify-center gap-3 items-center'>
+        <Image src={logoSrc} alt={`${provider} 로고`} width={logoWidth} height={logoHeight} style={{ width: `${logoWidth}px`, height: `${logoHeight}px` }}/>
+        <span className="font-semibold">{provider}로 로그인하기</span>
+      </div>
+    </button>
+  );
 }
-export default OAuthButton
+export default SocialLoginButton;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,32 +13,36 @@ export default function Page() {
       />
 
       <div className="relative z-10 flex flex-col items-center">
-        <Image
-          src="/images/logo.png"
-          alt="로고"
-          width={400}
-          height={200}
-        />
+        <Image src="/images/logo.png" alt="로고" width={400} height={400} />
         <Image
           src="/images/nuts.png"
           alt="너츠"
           width={300}
-          height={120}
-           className="absolute -bottom-30 left-1/2 -translate-x-1/2 rotate-30"
+          height={200}
+          className="absolute -bottom-30 left-1/2 -translate-x-1/2 rotate-30"
         />
       </div>
 
       <div className="absolute flex flex-col items-center bottom-10 z-10 w-full max-w-[600px] px-4 gap-3">
         <SocialLoginButton
-          provider="naver"
+          provider="네이버"
+          logoSrc="/images/naver-logo.png"
+          logoWidth={20}
+          logoHeight={20}
           className="bg-[#03C75A] text-white"
         />
         <SocialLoginButton
-          provider="kakao"
+          provider="카카오"
+          logoSrc="/images/kakao-logo.png"
+          logoWidth={20}
+          logoHeight={18}
           className="bg-[#FEE500] text-black"
         />
         <SocialLoginButton
-          provider="google"
+          provider="구글"
+          logoSrc="/images/google-logo.png"
+          logoWidth={20}
+          logoHeight={21}
           className="bg-white text-black"
         />
       </div>
