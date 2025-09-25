9935bc81cbc4e27c6ad7e6601a625bf0dc7b4963
skyunlee98 <skyunlee@naver.com>
2025-09-25 13:57:47 +0900

    feat: AlertModal 구현

--- src/app/(with-nav)/test/page.tsx
+++ src/app/(with-nav)/test/page.tsx
@@ -2,24 +2,32 @@
 
 import ContentWrapper from '@/components/layout/ContentWrapper';
 import Header from '@/components/layout/Header';
+import AlertModal from '@/components/modal/AlertModal';
 import ConfirmModal from '@/components/modal/ConfirmModal';
 import Toggle from '@/components/ui/Toggle';
 import { useState } from 'react';
 
 export default function Page() {
   const [checked, setChecked] = useState(false); // 토글 상태 관리
-  const [open, setOpen] = useState(false); // 모달 열림 상태
+  const [confirmOpen, setConfirmOpen] = useState(false); // ConfirmModal 열림 상태
+  const [alertOpen, setAlertOpen] = useState(false); // AlertModal 열림 상태
 
-  // 확인 버튼 눌렀을 때 동작
+  // 컨펌 확인 버튼 눌렀을 때 동작
   const handleConfirm = () => {
     console.log('확인 버튼 눌림');
-    setOpen(false);
+    setConfirmOpen(false);
   };
 
   // 취소 버튼 눌렀을 때 동작
   const handleCancel = () => {
     console.log('취소 버튼 눌림');
-    setOpen(false);
+    setConfirmOpen(false);
+  };
+
+  // 얼럿 확인 버튼 눌렀을 때 동작
+  const handleAlert = () => {
+    console.log('확인 버튼 눌림');
+    setAlertOpen(false);
   };
 
   return (
@@ -92,18 +100,34 @@ export default function Page() {
 
         <button
           type="button"
-          onClick={() => setOpen(true)}
+          onClick={() => setConfirmOpen(true)}
           className="mt-4 px-4 py-2 bg-blue-500 text-white rounded cursor-pointer"
         >
-          모달 열기
+          컨펌 모달 열기
+        </button>
+
+        <button
+          type="button"
+          onClick={() => setAlertOpen(true)}
+          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded cursor-pointer"
+        >
+          얼럿 모달 열기
         </button>
 
         <ConfirmModal
-          open={open}
+          open={confirmOpen}
           onConfirm={handleConfirm}
           onCancel={handleCancel}
           variant="happy"
           lines={['파티에 참여 하시겠습니까?']}
+        />
+
+        <AlertModal
+          open={alertOpen}
+          onConfirm={handleAlert}
+          title="나혼자 레벨업 ⚔️"
+          description="획득 조건 : 레벨 5렙 달성"
+          detail="“누구의 도움도 필요 없어! 혼자서도 충분히 강해지는 중.”"
         />
       </ContentWrapper>
     </>
