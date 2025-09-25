7a89b9eb543783ccf58655936b9b92cea252e368
taeeun98 <xodms6360@naver.com>
2025-09-26 01:09:36 +0900

    feat: 로그인 api 연동

--- src/app/login/components/SocialLoginButton.tsx
+++ src/app/login/components/SocialLoginButton.tsx
@@ -10,17 +10,34 @@ interface SocialLoginButtonProps {
   className: string;
 }
 
-function SocialLoginButton({ provider, logoSrc, logoWidth, logoHeight, className }: SocialLoginButtonProps) {
+function SocialLoginButton({
+  provider,
+  logoSrc,
+  logoWidth,
+  logoHeight,
+  className
+}: SocialLoginButtonProps) {
+  const providerMap: Record<Provider, string> = {
+    카카오: 'kakao',
+    네이버: 'naver',
+    구글: 'google'
+  };
+
   return (
-    // TODO : Link? api 호출? onclick? 어떻게 구현되는지 모르겠음 => api 명세서 이후에 작업
-    <button
-      className={`w-full max-w-[328px] h-[48px] rounded-lg shadow-[0_4px_4px_rgba(0,0,0,0.25)] cursor-pointer ${className}`}
+    // TODO: 리다이렉트 주소 로딩창으로 변경 -> 로딩창에서 첫로그인인지 api 검증 -> response에 따라 profile or home 으로 리다이렉트
+    <a
+      href={`http://localhost:8080/oauth2/authorization/${providerMap[provider]}?redirectUrl=http://localhost:3000/home`}
+      className={`w-full max-w-[328px] h-[48px] flex justify-center gap-3 items-center rounded-lg shadow-[0_4px_4px_rgba(0,0,0,0.25)] cursor-pointer ${className}`}
     >
-      <div className='flex justify-center gap-3 items-center'>
-        <Image src={logoSrc} alt={`${provider} 로고`} width={logoWidth} height={logoHeight} style={{ width: `${logoWidth}px`, height: `${logoHeight}px` }}/>
-        <span className="font-semibold">{provider}로 로그인하기</span>
-      </div>
-    </button>
+      <Image
+        src={logoSrc}
+        alt={`${provider} 로고`}
+        width={logoWidth}
+        height={logoHeight}
+        style={{ width: `${logoWidth}px`, height: `${logoHeight}px` }}
+      />
+      <span className="font-semibold">{provider}로 로그인하기</span>
+    </a>
   );
 }
 export default SocialLoginButton;
