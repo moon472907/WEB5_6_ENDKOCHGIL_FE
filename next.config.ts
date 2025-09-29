43910c6f9c8259038a405e0966dadb7a0c7676c2
taeeun98 <xodms6360@naver.com>
2025-09-29 14:45:19 +0900

    fix: api 주소 변경

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
 };
+
+// module.exports = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         destination: "http://localhost:8080/api/:path*",
+//       },
+//     ];
+//   },
+// };
 
 export default nextConfig;
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,20 +1,26 @@
 import ProfileForm from '@/components/profile/ProfileForm';
+import { getProfile } from '@/lib/api/member';
+import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 export default async function Page() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/members/valid`,
-    {
-      method: 'GET',
-      credentials: 'include',
-      cache: 'no-store'
+  const cookieStore = cookies();
+  const accessToken = (await cookieStore).get('accessToken')?.value;
+
+  try {
+    const data = await getProfile(accessToken);
+    console.log('getProfile data', data);
+
+    if (data.success && data.content.valid) {
+      console.log('getProfile valid true! ');
+      
+      redirect('/');
     }
-  );
-
-  const data = await res.json();
-
-  if (data.success && data.content.valid) {
-    redirect('/home');
+  } catch (error: unknown) {
+    // TODO : alert? modal? toast?
+    const e = error as Error;
+  if (e.message === 'NEXT_REDIRECT') throw e;
+  console.log('프로필 조회 에러 발생:', e.message);
   }
 
   return <ProfileForm />;
