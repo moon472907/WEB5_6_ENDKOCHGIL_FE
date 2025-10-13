e99af464853c4e32abfe06d58ed8a013f098051a
taeeun98 <xodms6360@naver.com>
2025-10-13 09:55:25 +0900

    test: 날짜 픽스 (테스트용)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,15 @@ import CharacterSection from './home/components/CharacterSection';
 import MissionListSection from './home/components/MissionListSection';
 import { getTodayTask } from '@/lib/api/home/task';
 import { getMyInfo } from '@/lib/api/member';
+import { setDevTime } from '@/lib/api/home/devTime';
 
 export default async function Home() {
   const cookieStore = cookies();
   const accessToken = (await cookieStore).get('accessToken')?.value;
   const tasks = await getTodayTask(accessToken);
   const profile = await getMyInfo(accessToken);
-
+  await setDevTime('2025-10-13');
+  
   return (
     <div className="flex flex-col min-h-dvh pb-20">
       <HeaderSection profile={profile} accessToken={accessToken} />
