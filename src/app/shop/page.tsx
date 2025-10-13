301c3bafce32e4fb37f8fb93d0da125fb09d1aaa
skyunlee98 <skyunlee@naver.com>
2025-10-13 11:42:33 +0900

    feat: 코인 api 연결

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,120 +1,20 @@
-'use client';
+import { cookies } from 'next/headers';
+import { redirect } from "next/navigation";
+import { getMyInfo } from '@/lib/api/member';
+import ShopClient from './components/ShopClient';
 
-import Header from '@/components/layout/Header';
-import Coin from '@/components/ui/Coin';
-import Image from 'next/image';
-import { useEffect, useRef, useState } from 'react';
-import TabBar from './components/TabBar';
-import CategoryTabs, { Category } from './components/CategoryTabs';
-import ItemGrid from './components/ItemGrid';
-import { Item, mockItems } from './items';
-import ConfirmModal from '@/components/modal/ConfirmModal';
-import { HiOutlineRefresh } from 'react-icons/hi';
+export default async function Page() {
+  try {
+    const cookieStore = cookies();
+    const accessToken = (await cookieStore).get('accessToken')?.value;
 
-export default function Page() {
-  const [tab, setTab] = useState<'shop' | 'closet'>('shop'); // 탭 상태 관리
-  const [category, setCategory] = useState<Category>('all'); // 카테고리 상태 관리
-  const [selectedItem, setSelectedItem] = useState<Item | null>(null); // 선택한 아이템 상태 관리
-  const listRef = useRef<HTMLDivElement | null>(null); // 스크롤 상태 관리
+    if (!accessToken) redirect("/login");
 
-  // 아이템 필터링
-  const filteredItems = mockItems.filter(
-    item =>
-      (tab === 'shop' ? !item.owned : item.owned) &&
-      (category === 'all' || item.category === category)
-  );
+    const profile = await getMyInfo(accessToken);
 
-  // 탭 바뀔 때 스크롤, 카테고리 초기화
-  useEffect(() => {
-    setCategory('all');
-    listRef.current?.scrollTo({ top: 0, behavior: 'auto' });
-  }, [tab]);
-
-  // 카테고리 바뀔 때 스크롤 초기화
-  useEffect(() => {
-    listRef.current?.scrollTo({ top: 0, behavior: 'auto' });
-  }, [category]);
-
-  // 모달 문구
-  const confirmLines =
-    selectedItem &&
-    (tab === 'shop'
-      ? [`[${selectedItem.name}]`, '구매하시겠습니까?']
-      : [`[${selectedItem.name}]`, '착용하시겠습니까?']);
-
-  // 컨펌 확인 버튼 눌렀을 때 동작
-  const handleConfirm = () => {
-    if (!selectedItem) return;
-    if (tab === 'shop') {
-      alert(`구매 완료`);
-    } else {
-      alert(`착용 완료`);
-    }
-    setSelectedItem(null);
-  };
-
-  return (
-    <div className="flex flex-col h-screen overflow-hidden pb-14">
-      {/* 헤더 영역 */}
-      <div className="relative flex-none">
-        <Header title={tab === 'shop' ? '상점' : '옷장'} bgColor="white" />
-        <div className="absolute right-4 top-0 h-14 flex items-center z-[60]">
-          <Coin coin={100} />
-        </div>
-      </div>
-
-      {/* 흰색 영역 */}
-      <section className="flex flex-col bg-basic-white py-4 px-5 gap-2 h-[calc(180px-56px)] flex-none"></section>
-
-      {/* 너츠 영역 */}
-      <section className="flex bg-nuts-floor px-5 items-center justify-end relative h-[120px] flex-none">
-        <Image
-          src="/images/nuts-kid.png"
-          alt="너츠"
-          width={110}
-          height={165}
-          priority
-          className="absolute -top-1/2 left-1/2 -translate-x-1/2 z-10"
-        />
-        {tab === 'closet' && (
-          <button
-            type="button"
-            className="cursor-pointer rounded-full bg-basic-white h-10 w-10"
-          >
-            <div className='text-basic-black flex flex-col items-center justify-center'>
-              <HiOutlineRefresh size={20} />
-            </div>
-          </button>
-        )}
-      </section>
-
-      {/* 아이템 영역 */}
-      <section className="flex flex-col flex-1 overflow-hidden">
-        <div className="flex-none">
-          <CategoryTabs category={category} setCategory={setCategory} />
-        </div>
-        <div ref={listRef} className="overflow-y-auto scrollbar flex-1">
-          <ItemGrid
-            items={filteredItems}
-            mode={tab}
-            onItemClick={setSelectedItem}
-          />
-        </div>
-      </section>
-
-      {/* 하단 탭 */}
-      <div className="flex-none">
-        <TabBar tab={tab} setTab={setTab} />
-      </div>
-
-      {/* 확인 모달 */}
-      <ConfirmModal
-        open={!!selectedItem}
-        onConfirm={handleConfirm}
-        onCancel={() => setSelectedItem(null)}
-        variant="happy"
-        lines={confirmLines ?? []}
-      />
-    </div>
-  );
+    return <ShopClient coin={profile.money} />;
+  } catch (err) {
+    console.error("회원 정보 로드 중 오류 발생:", err);
+    redirect("/login");
+  }
 }
